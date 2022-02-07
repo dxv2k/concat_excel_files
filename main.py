a08f5a4be819b378f9bd9f681cec378b70c765d4
dxv2k <9569076+dxv2k@users.noreply.github.com>
2022-02-07 11:21:31 +0700

    update new code: add tqdm, add columns (DiaChi, Tuoi)

--- main.py
+++ main.py
@@ -1,30 +1,72 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from tqdm import tqdm # progress bar  
+from datetime import datetime
 import pandas as pd
+import numpy as np 
 import os 
 
-DATA_PATH = "./data/"
+COLOR_YELLOW = '\033[33m'
+COLOR_GREEN = '\033[32m'
 
-file_list = os.listdir(DATA_PATH)
+DATA_PATH = "./data/"
+CURR_YEAR = datetime.now().year
+FILE_LIST = os.listdir(DATA_PATH)
 
 # columns used in merged file
-columns =  ["Mã BN","Họ và tên BN", "Năm sinh", "Giới Tính"]
+columns =  ["Mã BN","Họ và tên BN", "Năm sinh", "Giới Tính","Tuổi","Địa Chỉ"]
 
 # create empty dataframe with columns name
-df = pd.DataFrame(columns=[str(i) for i in columns])
+dataframe = pd.DataFrame(columns=[str(i) for i in columns])
 
-# copy from files to dataframe 
-for idx, file in enumerate(file_list): 
+total_patients = 0
+# copy from files to dataframe & add new columns   
+for file in tqdm(FILE_LIST, total=len(FILE_LIST)): 
     curr_data = pd.read_excel(DATA_PATH + file)
+    total_patients += len(curr_data)
 
     # get columns 1,2,3,4 (idx start at 0) 
     # mã BN, họ tên BN, năm sinh, giới tính 
+    address_data = curr_data.iloc[:,5:9]
     curr_data = curr_data.iloc[:,1:5]
 
     # remove headers 
     curr_data = curr_data[:].values
 
-    # convert to dataframe 
+    # add column age & append to list 
+    age_list = []
+    for row in curr_data: 
+        try: 
+            age_list.append(CURR_YEAR - int(row[2]))
+        except ValueError: 
+            age_list.append(0)
+    # convert & reshape to numpy vector 
+    age_list = np.asarray(age_list)
+    age_list = age_list.reshape((age_list.shape[0],1)) 
+
+    # add column address & append to list 
+    address_list = address_data.values.tolist() 
+    for idx,address in enumerate(address_list): 
+        address_list[idx] = ' '.join([val for val in address_list[idx] if isinstance(val,str)])
+    address_list = np.asarray(address_list)
+    address_list = address_list.reshape((address_list.shape[0],1)) 
+
+    # concate dataframe & age_list & address_list 
+    curr_data = np.hstack((curr_data,age_list,address_list))
+
+    # convert & append to dataframe 
     curr_data = pd.DataFrame(curr_data, columns=columns)
+    dataframe = dataframe.append(curr_data, ignore_index=True)
 
-    df = df.append(curr_data, ignore_index=True)
+# Check if matching 
+print("="*20)
+print(COLOR_YELLOW + "Accumulate patients in all files: ",total_patients)
+print(COLOR_YELLOW + "Total in output: ",len(dataframe))
+print("="*20)
 
-df.to_excel("./output.xlsx",sheet_name="Tổng hợp")
+print(COLOR_GREEN + "Saving result to file...")
+dataframe.to_excel("./output.xlsx",sheet_name="Tổng hợp")
+
+# keep command line open  
+input("Press any key to exit...")
